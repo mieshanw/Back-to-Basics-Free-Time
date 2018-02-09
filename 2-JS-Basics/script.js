c1d14ffdf9eeba26b6b8ae37b49100f481ac66f7
Miesha <mieshanw@gmail.com>
2018-02-09 13:40:16 -0500

    Add

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,7 +1,23 @@
-var name = 'Miesha';
-var lastName = 'Williams';
+//variables
+/*var lastName = 'Williams';
 var age = 23;
 var fullAge = true; 
 console.log(name + " "+lastName);
 console.log(age);
-console.log(fullAge);
+console.log(fullAge);*/
+
+var name = 'Miesha'; 
+var age = 23; 
+/*console.log(name+ " "+age);
+console.log(age+age);
+
+var job, isMarried; 
+
+console.log(job);*/
+
+job = "Full Stack Developer"; 
+isMarried = false; 
+
+console.log(name+ " "+ "is"+ " "+ "a"+" "+ age+ " "+ "year old "+ job); 
+console.log("Is she married?")
+console.log(isMarried);
